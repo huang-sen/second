4c26828f96da6eecd17307e7b620b7ad0eb38bfc
黄森 <1622290308@qq.com>
2019-09-29 12:03:13 +0800

    second

--- src/main/java/cn/yunhe/servlet/IndexServlet.java
+++ src/main/java/cn/yunhe/servlet/IndexServlet.java
@@ -22,7 +22,7 @@ public class IndexServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		
-		System.out.println("hello maven");
+		System.out.println("hello maven xxxx");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
